9ad71df792c6000e0dddecbe7ecec27e9acda725
igor feital <igfeitall@gmail.com>
2022-09-09 00:56:00 -0300

    getToken testing

---
+++ lambdas/__tests__/getToken.test.js
@@ -0,0 +1,76 @@
+const DynamoController = require('../../controllers/dynamoController')
+const getToken = require('../getToken')
+
+jest.mock('../../controllers/dynamoController')
+
+function mockDynamo(){
+  DynamoController.mockImplementation(() => {
+    return {
+      getById: async () => {
+        return {
+          Items: [
+            {
+              tokenId: 'BTC',
+              evolutionRate: 0.05,
+              timestamp: 1529571067,
+              exchangeRate: 50
+            }
+          ]
+        }
+      }
+    }
+  })
+}
+
+test('geting a history of a token', async () => {
+  const event = {pathParameters: {id: "BTC"}}
+
+  mockDynamo()
+
+  await getToken.handler(event, null, (error, msg) => {
+    expect(msg.statusCode).toBe(200)
+    expect(JSON.parse(msg.body)).toEqual([
+      {
+        tokenId: 'BTC',
+        evolutionRate: expect.any(String),
+        timestamp: expect.any(String),
+        exchangeRate: expect.any(Number)
+      }
+    ])
+
+  })
+})
+
+test('getting an erro by passing a non existent tokenId', async () => {
+  const event = {pathParameters: {id: "BTC"}}
+
+  DynamoController.mockImplementation(() => {
+    return {
+      getById: () => {
+        return {
+          Items:[
+            {
+              tokenId: 'ETH'
+            }
+          ]
+        }
+      }
+    }
+  })
+
+  expect(async () => await getToken.handler(event, null, (error, msg) => {
+    console.log(msg)
+  })).rejects.toThrow()
+
+})
+
+test('getting an exeption by not passing an id', async () => {
+  const event = {pathParameters: {}}
+
+  mockDynamo()
+
+  expect(async () => await getToken.handler(event, null, (error, msg) => {
+    console.log(msg)
+  })).rejects.toThrow()
+
+})
--- lambdas/getToken.js
+++ lambdas/getToken.js
@@ -13,12 +13,11 @@ async function getToken (event, context, callback) {
 
     validation.hasParam(event, 'id')
     const tokenId = String(event.pathParameters.id)
-    console.log(tokenId);
     
     // connection
     const tokenHistory = await dynamoDB.getById(tokenId)
-
     validation.arrayExist(tokenHistory.Items, tokenId)
+    
     const formatedTokenHistory = tokenHistory.Items.map( token => formatData(token))
     callback(null, response(200, formatedTokenHistory))
   } catch (err) {
